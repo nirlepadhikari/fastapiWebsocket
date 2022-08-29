7d5b445cccc8e7f964e62a6b19ad0a7dc6c7a8d5
nirlepadhikari <71162816+nirlepadhikari@users.noreply.github.com>
2022-08-29 15:34:43 +1000

    as

--- main.py
+++ main.py
@@ -9,13 +9,11 @@ app = FastAPI(title='WebSocket Example')
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
-# Create a fastapi websocket endpoint to send random numbers to the client
-
     await websocket.accept()
     while True:
-        await websocket.send_json(json.loads(pd.read_html("https://merolagani.com/LatestMarket.aspx")[0].to_json(orient="records")))
+        await websocket.send_json(json.loads(pd.read_html("https://merolagani.com/LatestMarket.aspx")[0].iloc[:, :-2].to_json(orient="records")))
         await asyncio.sleep(30)
 
 @app.get("/")
 async def root():
-    return json.loads(pd.read_html("https://merolagani.com/LatestMarket.aspx")[0].to_json(orient="records"))
+    return json.loads(pd.read_html("https://merolagani.com/LatestMarket.aspx")[0].iloc[:, :-2].to_json(orient="records"))
